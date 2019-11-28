71d9c3076bd5400dd8d8db9671b399749eec0c56
Becky Carter <becky.carter@softwire.com>
2019-11-28 11:05:08 +0000

    Piece can move two spaces on first turn

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -15,11 +15,17 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        ArrayList<Move> moves =  new ArrayList<>();
+        ArrayList<Move> moves = new ArrayList<>();
         if (this.getColour() == PlayerColour.WHITE) {
+            if (from.getRow() == 6) {
+                moves.add(new Move(from, from.plus(-2, 0)));
+            }
             moves.add(new Move(from, from.plus(-1, 0)));
         }
         if (this.getColour() == PlayerColour.BLACK) {
+            if (from.getRow() == 1) {
+                moves.add(new Move(from, from.plus(2, 0)));
+            }
             moves.add(new Move(from, from.plus(1, 0)));
         }
         return moves;
