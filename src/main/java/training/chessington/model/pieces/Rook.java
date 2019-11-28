9f78bf21563c6e45e0002c7473a19d6312fc2293
Becky Carter <becky.carter@softwire.com>
2019-11-28 13:45:12 +0000

    Rook can move on empty board

--- src/main/java/training/chessington/model/pieces/Rook.java
+++ src/main/java/training/chessington/model/pieces/Rook.java
@@ -15,6 +15,49 @@ public class Rook extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        ArrayList<Move> moves = new ArrayList<>();
+        moves.addAll(getVerticalMoves(from, board));
+        moves.addAll(getHorizontalMoves(from, board));
+        return moves;
+    }
+
+    private ArrayList<Move> getVerticalMoves(Coordinates from, Board board) {
+        ArrayList<Move> moves = new ArrayList<>();
+
+        for (int currRow = from.getRow() + 1; currRow <= 7; currRow++) {
+            if (checkMove(from, board, moves, currRow, from.getCol())) break;
+        }
+
+        for (int currRow = from.getRow() - 1; currRow >= 0; currRow--) {
+            if (checkMove(from, board, moves, currRow, from.getCol())) break;
+        }
+
+        return moves;
+    }
+
+    private ArrayList<Move> getHorizontalMoves(Coordinates from, Board board) {
+        ArrayList<Move> moves = new ArrayList<>();
+
+        for (int currCol = from.getCol() + 1; currCol <= 7; currCol++){
+            if (checkMove(from, board, moves, from.getRow(), currCol)) break;
+        }
+
+        for (int currCol = from.getCol() -1; currCol >=0; currCol--) {
+            if (checkMove(from, board, moves, from.getRow(), currCol)) break;
+        }
+
+        return moves;
+    }
+
+    private boolean checkMove(Coordinates from, Board board, ArrayList<Move> moves, int currRow, int currCol) {
+        Coordinates coords = new Coordinates(currRow, currCol);
+        Piece piece = board.get(coords);
+
+        if (piece == null) moves.add(new Move(from, coords));
+        else {
+            if (piece.getColour() != this.getColour()) moves.add(new Move(from, coords));
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/training/chessington/model/pieces/RookTest.java
+++ src/test/java/training/chessington/model/pieces/RookTest.java
@@ -1,7 +1,88 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class RookTest {
+    @Test
+    public void whiteRookCanMoveVerticallyOnEmptyBoard() {
+        // Arrange
+        Board board = Board.empty();
+
+        Piece rook = new Rook(PlayerColour.WHITE);
+        Coordinates coordinates = new Coordinates(7, 0);
+        board.placePiece(coordinates, rook);
+
+        // Act
+        List<Move> moves = rook.getAllowedMoves(coordinates, board);
+
+        // Assert
+        for (int i = -1; i >= -7; i--) {
+            assertThat(moves).contains(new Move(coordinates, coordinates.plus(i, 0)));
+        }
+    }
+
+    @Test
+    public void blackRookCanMoveVerticallyOnEmptyBoard() {
+        // Arrange
+        Board board = Board.empty();
+
+        Piece rook = new Rook(PlayerColour.BLACK);
+        Coordinates coordinates = new Coordinates(0, 0);
+        board.placePiece(coordinates, rook);
+
+        // Act
+        List<Move> moves = rook.getAllowedMoves(coordinates, board);
+
+        // Assert
+        for (int i = 1; i <= 7; i++) {
+            assertThat(moves).contains(new Move(coordinates, coordinates.plus(i, 0)));
+        }
+    }
+
+    @Test
+    public void whiteRookCanMoveHorizontallyOnEmptyBoard() {
+        // Arrange
+        Board board = Board.empty();
+
+        Piece rook = new Rook(PlayerColour.WHITE);
+        Coordinates coordinates = new Coordinates(7, 0);
+        board.placePiece(coordinates, rook);
+
+        // Act
+        List<Move> moves = rook.getAllowedMoves(coordinates, board);
+
+        // Assert
+        for (int i = 1; i <= 7; i++) {
+            assertThat(moves).contains(new Move(coordinates, coordinates.plus(0, i)));
+        }
+    }
+
+    @Test
+    public void blackRookCanMoveHorizontallyOnEmptyBoard() {
+        // Arrange
+        Board board = Board.empty();
+
+        Piece rook = new Rook(PlayerColour.BLACK);
+        Coordinates coordinates = new Coordinates(0, 0);
+        board.placePiece(coordinates, rook);
+
+        // Act
+        List<Move> moves = rook.getAllowedMoves(coordinates, board);
+
+        // Assert
+        for (int i = 1; i <= 7; i++) {
+            assertThat(moves).contains(new Move(coordinates, coordinates.plus(0, i)));
+        }
+    }
+
+
 }
