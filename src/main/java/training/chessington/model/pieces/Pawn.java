f1685818919372e944e5a6e03d6bd7a376a02f98
Becky Carter <becky.carter@softwire.com>
2019-11-28 12:06:12 +0000

    Refactor pawn movement, storing direction based on colour.

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -9,8 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn extends AbstractPiece {
+    private int direction;
     public Pawn(PlayerColour colour) {
         super(Piece.PieceType.PAWN, colour);
+        if (colour.equals(PlayerColour.WHITE)) direction = -1;
+        if (colour.equals(PlayerColour.BLACK)) direction = 1;
     }
 
     @Override
@@ -25,19 +28,11 @@ public class Pawn extends AbstractPiece {
         ArrayList<Move> moves = new ArrayList<>();
         ArrayList<Coordinates> potentialCoords = new ArrayList<>();
 
-        if (this.getColour() == PlayerColour.WHITE) {
-            if (from.getRow() == 6) {
-                potentialCoords.add(from.plus(-2, 0));
-            }
-            potentialCoords.add(from.plus(-1, 0));
+        if ((this.getColour() == PlayerColour.WHITE && from.getRow() == 6)
+                || (this.getColour() == PlayerColour.BLACK && from.getRow() == 1 )) {
+            potentialCoords.add(from.plus(direction * 2, 0));
         }
-
-        if (this.getColour() == PlayerColour.BLACK) {
-            if (from.getRow() == 1) {
-                potentialCoords.add(from.plus(2, 0));
-            }
-            potentialCoords.add(from.plus(1, 0));
-        }
+        potentialCoords.add(from.plus(direction, 0));
 
         potentialCoords.forEach(coordinates -> {
             if (coordinates.isOnBoard()
